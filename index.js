8d3253f2a88a1b6af0486503678c6aaa06518f96
arpanpatil66 <107909839+arpanpatil66@users.noreply.github.com>
2022-06-23 13:36:49 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./Asignment/App";
-import "./Asignment/Styles.css";
-
-ReactDom.render(<App />, document.getElementById("root"));
